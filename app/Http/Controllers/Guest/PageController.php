672bae3b8f97de2f8bb36280b7b52c9eea0312b6
Marco Rinaldi <marcorinaldi98@libero.it>
2023-05-12 15:12:33 +0200

    visualizzazione treni in partenza oggi

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        $trains = Train::where('orario_di_partenza', '>=', date('Y-m-d'))->get();
 
         return view('home', compact('trains'));
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,16 +6,40 @@
 
     <div class="container-lg">
         <h1 class="text-primary py-5">Lista Treni</h1>
-        <ul>
-            @foreach ($trains as $train)
-                <li class="py-2">
-                    {{ $train->id }} - {{ $train->azienda }} - {{ $train->stazione_di_partenza }} -
-                    {{ $train->stazione_di_arrivo }} - {{ $train->orario_di_partenza }} - {{ $train->orario_di_arrivo }}
-                    - {{ $train->codice_treno }} - {{ $train->carrozze }} - {{ $train->in_orario }} -
-                    {{ $train->cancellato }}
-                </li>
-            @endforeach
-        </ul>
+        <table class="table table-striped">
+            <thead>
+                <tr class="table-dark">
+                    <th class="text-primary" scope="col">ID</th>
+                    <th class="text-primary" scope="col">Azienda</th>
+                    <th class="text-primary" scope="col">Stazione di partenza</th>
+                    <th class="text-primary" scope="col">Stazione di arrivo</th>
+                    <th class="text-primary" scope="col">Orario di partenza</th>
+                    <th class="text-primary" scope="col">Orario di arrivo</th>
+                    <th class="text-primary" scope="col">Codice Treno</th>
+                    <th class="text-primary" scope="col">Numero Carrozze</th>
+                    <th class="text-primary" scope="col">In orario</th>
+                    <th class="text-primary" scope="col">Cancellato</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                    <tr>
+                        <th class="text-primary" scope="row">{{ $train->id }}</th>
+                        <td class="text-info">{{ $train->azienda }}</td>
+                        <td>{{ $train->stazione_di_partenza }}</td>
+                        <td>{{ $train->stazione_di_arrivo }}</td>
+                        <td>{{ $train->orario_di_partenza }}</td>
+                        <td>{{ $train->orario_di_arrivo }}</td>
+                        <td class="text-uppercase text-warning">{{ $train->codice_treno }}</td>
+                        <td class="text-center">{{ $train->carrozze }}</td>
+                        <td class="text-center fw-bold {{ $train->in_orario ? 'text-success' : 'text-danger' }}">
+                            {{ $train->in_orario }}</td>
+                        <td class="text-center fw-bold {{ $train->cancellato ? 'text-success' : 'text-danger' }}">
+                            {{ $train->cancellato }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 
 @endsection
